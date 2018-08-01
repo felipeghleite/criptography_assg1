a2bb11bfbd034df01e2524b333c86f84247ae869
fleite <felipe.leite@gertec.com.br>
2018-08-01 12:26:39 -0300

    commit

--- assignment2.c
+++ assignment2.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void main(){
 	unsigned short ch;
@@ -6,13 +7,14 @@ void main(){
 	int i = 0;
 	unsigned int keyLength;
 	unsigned short initCh;
+	unsigned int temp;
 
 	fpOut = fopen("freq.txt", "w");
 	
 	for(keyLength = 2; keyLength < 10 ; keyLength++){
 
         	fpIn = fopen("ctext.txt", "r");
-		fscanf(fpIn,"%02X", &initCh);
+        	temp = fscanf(fpIn,"%02X", &initCh);
 		fseek(fpIn, keyLength, SEEK_CUR);
 		
 		while(fscanf(fpIn, "%02X", &ch) != EOF){
