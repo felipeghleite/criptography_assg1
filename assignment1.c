5d0d9a6bd5b535c8429e2f046e430d1f3b78bc69
fleite <felipe.leite@gertec.com.br>
2018-08-06 18:14:50 -0300

    key cracked succesfully

--- assignment1.c
+++ assignment1.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define KEY_LENGTH 5
+#define KEY_LENGTH 7
 
 void main(){
 	unsigned short ch = 0;
 	FILE *fpIn, *fpOut;
 	int i;
-	unsigned char key[KEY_LENGTH] = {0x5A, 0x1B, 0x00, 0x00, 0x00};
-
+//	unsigned char key[KEY_LENGTH] = {0xBA, 0x20, 0x80, 0xA0, 0x00, 0x80, 0x20};
+	unsigned char key[KEY_LENGTH] = {0xBA, 0x1F, 0x91, 0xB2, 0x53, 0xCD, 0x3E};
 	fpIn = fopen("ctext.txt", "r+");
 	if(fpIn == NULL ){
 			printf("erro");
--- assignment2.c
+++ assignment2.c
@@ -11,8 +11,9 @@ void main(){
 	unsigned short initCh;
 	unsigned int temp, temp1;
 	unsigned int lastMax, lastKeyLength, lastPos;
-	unsigned char keyTemp, keyIndex;
-	unsigned char key[5] = {0x00, 0x00, 0x00, 0x00, 0x00};
+	unsigned short keyTemp, keyIndex;
+	unsigned char key[32] = {0};
+	unsigned char keyFinal[32] = {0};
 	float qi2, qimax, qi;
 	lastMax = 0;
 	lastKeyLength = 0;
@@ -27,64 +28,86 @@ void main(){
 	fileLength = (unsigned int)ftell(fpIn);
 	fclose(fpIn);
 
-	for(keyLength = 2; keyLength < 17 ; keyLength++){
-		
-			for(temp1 = 0x00 ; temp1 < 0x100; temp1++){
-				qi = 0;
-				for(i = 0; i < keyLength+1; i++){
+	for(keyLength = 4; keyLength < 10 ; keyLength++){
+
+		for(temp1 = 0x00 ; temp1 < 0x100; temp1++){
+			qi = 0;
+			for(i = 0; i < keyLength+1; i++){
 				fpIn = fopen("ctext.txt", "r");
-				fseek(fpIn, i, SEEK_CUR);
+				fseek(fpIn, i*2, SEEK_CUR);
 
-									while(fscanf(fpIn, "%02X", &ch) != EOF){
-										if(temp1  == ch)
-										{
-											qi++;
-										}
-										fseek(fpIn, keyLength, SEEK_CUR);
-									}
-
-									fclose(fpIn);
+				while(fscanf(fpIn, "%02X", &ch) != EOF){
+					if(temp1  == ch)
+					{
+						qi++;
 					}
+					fseek(fpIn, (keyLength-1)*2, SEEK_CUR);
+				}
+
+				fclose(fpIn);
 			}
 			qi = qi/256;
 			qi2 += qi*qi;
+		}
+		if(qi2 > qimax){
+			qimax = qi2;
+			lastKeyLength = keyLength;
+		}
+		qi2 = 0;
+	}
+
+	j = 0;
+	qi2 = 0;
+	qimax = 0;
+
+	for(keyIndex = 0; keyIndex < lastKeyLength+1; keyIndex++){
+
+		for(keyTemp = 0; keyTemp < 257; keyTemp++){
+			key[keyIndex] = keyTemp;
+			j = 1;
+			fpIn = fopen("ctext.txt", "r");
+			fpOut = fopen("dec_text.txt","w+");
+			fseek(fpIn, keyIndex*2, SEEK_SET);
+
+			while(fscanf(fpIn, "%02X", &ch) != EOF){
+
+				ch ^= key[keyIndex];
+				if(ch < 32 || ch > 127 ){
+					j = 0;
+					break;
+				}
+				fprintf(fpOut,"%c", ch);
+				fseek(fpIn, (lastKeyLength-1)*2, SEEK_CUR);
+			}
+			fclose(fpIn);
+			fclose(fpOut);
+
+			if(j){
+				j=0;
+				for(i = 32; i < 128; i++){
+					qi = 0;
+					fpOut = fopen("dec_text.txt", "r+");
+					while(fscanf(fpOut, "%c", &ch) != EOF){
+						if(ch == i){
+							qi++;
+						}
+					}
+					fclose(fpOut);
+					qi = qi/256;
+					qi2 += qi*qi;
+				}
 			if(qi2 > qimax){
-					qimax = qi2;
-					lastKeyLength = keyLength;
+				qimax = qi2;
+				keyFinal[keyIndex] = keyTemp;
 			}
 			qi2 = 0;
-	}
-	j=0;	
-	for(keyIndex = 0; keyIndex < lastKeyLength+1; keyIndex++){
-	
-		for(keyTemp = 0; keyTemp < 257; keyTemp++){
-				key[keyIndex] = keyTemp;
-				j = 1;		
-				fpIn = fopen("ctext.txt", "r");
-				fpOut = fopen("dec_text.txt","w+");
-
-				fseek(fpIn, keyIndex, SEEK_SET);
-
-				while(fscanf(fpIn, "%02X", &ch) != EOF){
-
-					ch ^= key[keyIndex];
-					if(ch < 32 || ch > 127 ){
-							j = 0; 
-							break;
-					}
-					fprintf(fpOut,"%c", ch);
-					fseek(fpIn, (lastKeyLength-1)*2, SEEK_CUR);
-				}
-				if(j){
-						j = 0;
-				}	
-				fclose(fpIn);
-				fclose(fpOut);	
 		}
 	}
+		qimax = 0;
+}
 
-	getchar();
-	return;
+getchar();
+return;
 }
 
 
